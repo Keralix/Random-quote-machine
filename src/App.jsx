9f208b4f2ad6d605d061b975aa1d2818d832305e
Tomislav Bračić <tbracic43@gmail.com>
2024-12-02 20:08:06 +0100

    Added styling to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import qutoes from './quotes.json'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons/faQuoteLeft'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 const getRandomQuote=()=>{
   return qutoes[Math.floor(Math.random()*qutoes.length)]
 }
@@ -12,15 +16,22 @@ function App() {
   return (
     <div className="background">
        <div id="quote-box">
-      <h1 id="text">
-        {quote.quote}
-      </h1>
-      <h4 id="author">
-        {quote.author}
-      </h4>
-      <button id="new-quote" onClick={handleButtonClick}>Get new quote</button>
-      <a href="twitter.com/intent/tweet" id="tweet-quote" target="_top" >tweet</a>
-     </div> 
+        <div id="quote">   
+          <FontAwesomeIcon icon={faQuoteLeft} size='2x'/>     
+          <h1 id="text">
+            {quote.quote}
+          </h1>
+          <FontAwesomeIcon icon={faQuoteRight} size='2x'/>
+        </div>
+
+        <h4 id="author">
+          {quote.author}
+        </h4>
+        <div className="buttons">
+          <button id="new-quote" onClick={handleButtonClick}>Get new quote</button>
+          <a href="twitter.com/intent/tweet" id="tweet-quote" target="_top" ><FontAwesomeIcon icon={faTwitter} size='3x'/></a>
+        </div>
+      </div> 
     </div>
     
   )
